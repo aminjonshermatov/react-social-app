e7bdab57be9f47f89700fd43e2289676de9a0d11
Aminjon <aminjonshermatov@gmail.com>
2020-11-03 14:20:57 +0500

    added photo input

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -19,9 +19,17 @@ export default function PostForm({onSave}) {
 
     const handleSubmit = ev => {
         ev.preventDefault();
+        let photo = null;
+        const [,,url,alt] = ev.target.childNodes;
+        if (url.value !== '') {
+            photo = {
+                url: url.value,
+                alt: alt.value
+            };
+        }
         let tags = post.tags?.filter(tag => tag !== '').map(tag => tag.replace(/^#/im, ''));
         if (tags[0] === undefined) tags = null;
-        onSave({...post, tags});
+        onSave({...post, tags, photo});
     };
 
     const handleChange = ev => {
@@ -38,7 +46,9 @@ export default function PostForm({onSave}) {
     return (
         <form onSubmit={handleSubmit}>
             <textarea name='content' value={post.content} onChange={handleChange}></textarea>
-            <input name='tags' value={post.tags?.join(' ')} onChange={handleChange}/>
+            <input name='tags' placeholder='tags' value={post.tags?.join(' ')} onChange={handleChange} />
+            <input name='photo' placeholder='photo'  />
+            <input name='alt' placeholder='alt' />
             <button>Ok</button>
         </form>
     );
